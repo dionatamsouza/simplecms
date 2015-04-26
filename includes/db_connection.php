8a99d28b2985049ce8ce31f252709febcc05691f
Sam <cdahal2@uic.edu>
2015-04-25 19:10:58 -0500

    change database login information

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 	define("DB_SERVER", "localhost");
-	define("DB_USER", "cowmax");
-	define("DB_PASS", "cowmax");
-	define("DB_NAME", "cowmax");
+	define("DB_USER", "superadmin");
+	define("DB_PASS", "superadmin");
+	define("DB_NAME", "simplecms");
 
   // 1. Create a database connection
   $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);

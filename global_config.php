1b2be7b2d42ced35a5255dd4b17b9ffae81ea901
Sam <cdahal2@uic.edu>
2015-04-25 22:16:40 -0500

    Consolidate database connection and directory path to global_config.php

--- global_config.php
+++ global_config.php
@@ -1,27 +1,42 @@
 <?php 
 
-//Extension of files
-@define(EXTENSION, ".php");
+// Database connections stuffs
+define("DB_SERVER", "localhost"); // DATABASE HOST
+define("DB_USER", "superadmin");  // DATABASE USERNAME
+define("DB_PASS", "superadmin");  // DATABASE PASSWORD
+define("DB_NAME", "simplecms");  // DATABASE NAME
 
-// get the parent directory cowmax in this case
-@define(BASE_DIR, basename(dirname($_SERVER['PHP_SELF']))); 
+// 1. Create a database connection
+$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+// Test if connection succeeded
+if(mysqli_connect_errno()) {
+	die("Database connection failed: " . 
+	mysqli_connect_error() . 
+	" (" . mysqli_connect_errno() . ")"
+	);
+}
 
-// Get include dir path
-@define(INC_PATH, BASE_DIR . '/includes/');
 
-// Get layout dir path
-@define(LAYOUT_PATH, INC_PATH . 'layouts/');
+define("PARENT_DIR", $_SERVER["DOCUMENT_ROOT"] . "/simplecms"); // Root Directory
+define("INC_DIR", PARENT_DIR . "/includes");
+define("PUBLIC_DIR", PARENT_DIR . "/public");
 
-//layout file path
-@define(LAYOUT_HTML_HEADER_PATH, INC_PATH . LAYOUT_PATH.'header'.EXTENSION);
-@define(LAYOUT_HTML_FOOTER_PATH, INC_PATH . LAYOUT_PATH.'footer'.EXTENSION);
+if($layout_context == "admin"){
+	define("LAYOUT_DIR", INC_DIR . "/layouts/admin");
+}else{
+	define("LAYOUT_DIR", INC_DIR . "/layouts");
+}
 
-// layout admin path
-@define(LAYOUT_ADMIN_PATH, LAYOUT_PATH.'admin/');
+// DEFINE Documents path
+define("FILE_EXTENSION",".php");
+define("SLASH","/");
 
-//Layout admin file path
-@define(LAYOUT_HTML_ADMIN_HEADER_PATH, INC_PATH . LAYOUT_ADMIN_PATH.'header'.EXTENSION);
-@define(LAYOUT_HTML_ADMIN_FOOTER_PATH, INC_PATH . LAYOUT_ADMIN_PATH.'footer'.EXTENSION);
+// Lists of file path
+define("FUNCTIONS_PATH", INC_DIR . SLASH . "functions" . FILE_EXTENSION);
+define("SESSION_PATH", INC_DIR . SLASH . "session" . FILE_EXTENSION);
+define("HEADER_PATH", LAYOUT_DIR . SLASH . "header" . FILE_EXTENSION);
+define("FOOTER_PATH",  LAYOUT_DIR . SLASH . "footer" . FILE_EXTENSION);
+define("VALIDATION_FUNCTIONS", INC_DIR . SLASH . "validation_functions" . FILE_EXTENSION);
 
 
 ?>
